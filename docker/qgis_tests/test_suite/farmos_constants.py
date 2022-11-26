0454ad17361233124caf8a9fa10cee7bc166e90b
Symbioquine <symbioquine@gmail.com>
2022-11-26 12:24:26 -0800

    Fix tests by changing from broken openid scope to farm_manager scope

--- docker/qgis_tests/test_suite/farmos_constants.py
+++ docker/qgis_tests/test_suite/farmos_constants.py
@@ -2,6 +2,6 @@ WFS_ENDPOINT = 'http://www/wfs'
 
 OAUTH_ENDPOINT = 'http://www/oauth/token'
 OAUTH_CLIENT_ID = 'farm'
-OAUTH_SCOPE = 'openid'
+OAUTH_SCOPE = 'farm_manager'
 OAUTH_USERNAME = 'root'
 OAUTH_PASSWORD = 'test'
